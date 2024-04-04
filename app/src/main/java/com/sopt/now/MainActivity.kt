ce77e991241b8d2fb8c3021934caf99acc4b66ac
youjin09222 <youjin09222@gmail.com>
2024-04-04 14:58:18 +0900

    feat/#1: 사용자 정보 세팅

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.sopt.now
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setUserInfo()
+    }
+
+    // 사용자 정보 세팅
+    private fun setUserInfo(){
+        val userId = intent.getStringExtra("userId")
+        val userPw = intent.getStringExtra("userPw")
+        val userName = intent.getStringExtra("userName")
+
+        binding.userId.text = userId
+        binding.userPw.text = userPw
+        binding.tvName.text = userName
     }
 }
