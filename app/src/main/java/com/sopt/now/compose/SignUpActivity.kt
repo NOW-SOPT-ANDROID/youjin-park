ec10f6edd414098503618ffcd0defc2900f04b78
youjin09222 <youjin09222@gmail.com>
2024-04-05 09:28:22 +0900

    feat/#3: 회원 가입 가능 여부 체크

--- app/src/main/java/com/sopt/now/compose/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/compose/SignUpActivity.kt
@@ -1,6 +1,8 @@
 package com.sopt.now.compose
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -23,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,6 +42,7 @@ class SignUpActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    SignUpCompose()
                 }
             }
         }
@@ -47,13 +51,16 @@ class SignUpActivity : ComponentActivity() {
 
 @Composable
 fun SignUpCompose(){
+    val context = LocalContext.current
     var userId by remember { mutableStateOf("") }
     var userPw by remember { mutableStateOf("") }
     var userName by remember { mutableStateOf("") }
     var userMbti by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(horizontal = 30.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 30.dp),
     ) {
         Spacer(modifier = Modifier.height(60.dp))
         Text(
@@ -75,7 +82,7 @@ fun SignUpCompose(){
             onValueChange = { userId = it },
             modifier = Modifier.fillMaxWidth(),
             label = { Text("아이디를 입력하세요.") },
-            singleLine = true, //단일 줄로 제한
+            singleLine = true //단일 줄로 제한
         )
         Spacer(modifier = Modifier.height(30.dp))
         Text(
@@ -89,7 +96,7 @@ fun SignUpCompose(){
             onValueChange = { userPw = it },
             modifier = Modifier.fillMaxWidth(),
             label = { Text("비밀번호를 입력하세요.") },
-            singleLine = true, //단일 줄로 제한
+            singleLine = true
         )
         Spacer(modifier = Modifier.height(10.dp))
         Spacer(modifier = Modifier.height(30.dp))
@@ -104,7 +111,7 @@ fun SignUpCompose(){
             onValueChange = { userName = it },
             modifier = Modifier.fillMaxWidth(),
             label = { Text("닉네임을 입력하세요.") },
-            singleLine = true, //단일 줄로 제한
+            singleLine = true
         )
         Spacer(modifier = Modifier.height(30.dp))
         Text(
@@ -115,14 +122,16 @@ fun SignUpCompose(){
         Spacer(modifier = Modifier.height(10.dp))
         TextField(
             value = userMbti,
-            onValueChange = { userPw = it },
+            onValueChange = { userMbti = it },
             modifier = Modifier.fillMaxWidth(),
             label = { Text("MBTI를 입력하세요.") },
-            singleLine = true, //단일 줄로 제한
+            singleLine = true
         )
         Spacer(modifier = Modifier.weight(2f))
         Button(
-            onClick = { /* 클릭 시 수행될 동작 */ },
+            onClick = {
+                checkSignUp(context, userId, userPw, userName, userMbti)
+            },
             colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
             shape = RoundedCornerShape(50.dp),
             modifier = Modifier.fillMaxWidth()
@@ -131,6 +140,25 @@ fun SignUpCompose(){
         }
         Spacer(modifier = Modifier.padding(bottom = 60.dp))
     }
+}
+
+// 회원 가입 가능 여부 체크
+fun checkSignUp(context: Context, userId: String, userPw: String, userName: String, userMbti: String) {
+    val isValidId = userId.length in 6..10
+    val isValidPw = userPw.length in 8..12
+    val isValidName = userName.trim().isEmpty() // 공백으로만 이루어진 경우 판단
+    val isEmpty = userId.isEmpty() || userPw.isEmpty() || userName.isEmpty() || userMbti.isEmpty()
+
+    val message = when {
+        !isValidId -> "ID는 6~10 글자여야 합니다."
+        !isValidPw -> "Password는 8~12 글자여야 합니다."
+        isValidName -> "공백으로만 이루어진 닉네임은 불가합니다."
+        isEmpty -> "모든 정보를 입력해주세요."
+        else -> {
+            "회원가입 성공!"
+        }
+    }
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
 
 @Preview(showBackground = true)
