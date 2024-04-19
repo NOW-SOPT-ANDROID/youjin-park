3e7e874527aeb9b87fa842c2fe52612528060891
youjin09222 <youjin09222@gmail.com>
2024-04-19 18:49:19 +0900

    add/#5: View Type 추가

--- app/src/main/java/com/sopt/now/test/friend/Friend.kt
+++ app/src/main/java/com/sopt/now/test/friend/Friend.kt
@@ -6,4 +6,9 @@ data class Friend(
     @DrawableRes val profileImage: Int,
     val name: String,
     val selfDescription: String,
-)
+) {
+    companion object {
+        const val TYPE_USER = 0
+        const val TYPE_FRIEND = 1
+    }
+}
