9a19af3006f1087e5bda51f9b2e16cbeabcfc46e
youjin09222 <youjin09222@gmail.com>
2024-04-19 23:33:41 +0900

    mod/#6: Main UI를 MyPage로 이동

---
+++ app/src/main/java/com/sopt/now/compose/MyPageView.kt
@@ -0,0 +1,82 @@
+package com.sopt.now.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
+
+@Composable
+fun MyPageView(userId: String, userPw: String, userName: String, userDescription: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 30.dp, vertical = 60.dp),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.iv_user_profile),
+                contentDescription = stringResource(id = R.string.app_name),
+                Modifier
+                    .size(100.dp)
+                    .aspectRatio(1f)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                text = userName,
+                fontSize = 20.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(60.dp))
+        UserInfoItem(
+            label = stringResource(id = R.string.tv_id),
+            value = userId
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        UserInfoItem(
+            label = stringResource(id = R.string.tv_pw),
+            value = userPw
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        UserInfoItem(
+            label = stringResource(id = R.string.tv_user_description),
+            value = userDescription
+        )
+    }
+}
+
+@Composable
+fun UserInfoItem(label: String, value: String) {
+    Column {
+        Text(
+            text = label,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = value,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyPagePreview() {
+    NOWSOPTAndroidTheme {
+        MyPageView("myPageSopt", "mypagePassword123", "MyPageUserName", "INFJ 입니다!")
+    }
+}
