2cdd3f77a37cc7a812b79045a0772895323126c8
youjin09222 <youjin09222@gmail.com>
2024-04-27 06:13:14 +0900

    refactor/#5: LoginActivity에 viewModel 적용

--- app/src/main/java/com/sopt/now/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/LoginActivity.kt
@@ -6,32 +6,39 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.sopt.now.databinding.ActivityLoginBinding
 import com.sopt.now.test.data.UserData
+import com.sopt.now.test.data.UserViewModel
+import com.sopt.now.test.data.UserViewModelFactory
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private var userData: UserData? = null
+    private lateinit var viewModel: UserViewModel
+    private var userData: UserData ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        clickLoginButton()
-        clickSignUpTextView()
+        val factory = UserViewModelFactory(this)
+        viewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
+
+        setupLoginButton()
+        setupSignUpTextView()
     }
 
     // 로그인
-    private fun clickLoginButton() {
+    private fun setupLoginButton() {
         binding.btnLogin.setOnClickListener {
-            checkLogin()
+            userData?.let { checkLogin(it) }
         }
     }
 
     // 회원가입 페이지로 이동
-    private fun clickSignUpTextView() {
+    private fun setupSignUpTextView() {
         binding.tvSignUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             signUpLauncher.launch(intent) // ActivityResultLauncher 실행
@@ -55,18 +62,16 @@ class LoginActivity : AppCompatActivity() {
     }
 
     // 로그인
-    private fun checkLogin(){
-        userData?.let { userData ->
-            val inputId = binding.etLoginId.text.toString()
-            val inputPw = binding.etLoginPw.text.toString()
+    private fun checkLogin(userData: UserData) {
+        val inputId = binding.etLoginId.text.toString()
+        val inputPw = binding.etLoginPw.text.toString()
 
-            // 아이디, 비번 확인
-            if (inputId == userData.userId && inputPw == userData.userPw) {
-                moveToMain(userData)
-            }
-            else {
-                Toast.makeText(this, "아이디 또는 비밀번호가 잘못되었습니다.", Toast.LENGTH_SHORT).show()
-            }
+        // 아이디, 비번 확인
+        if (inputId == userData.userId && inputPw == userData.userPw) {
+            showToast("로그인 성공!")
+            moveToMain(userData)
+        } else {
+            showToast("아이디 또는 비밀번호가 잘못되었습니다.")
         }
     }
 
@@ -76,6 +81,10 @@ class LoginActivity : AppCompatActivity() {
             putExtra("userData", userData)
         }
         startActivity(intent)
-        Toast.makeText(this, "로그인 성공!", Toast.LENGTH_SHORT).show()
+        finish()
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
