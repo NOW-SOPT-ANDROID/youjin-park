70c7e82fb27c84b5365247dd3cd8bd1d5172f7c2
youjin09222 <youjin09222@gmail.com>
2024-05-02 05:12:44 +0900

    refactor/#5: FriendList에 사용자 정보 추가

--- app/src/main/java/com/sopt/now/test/HomeFragment.kt
+++ app/src/main/java/com/sopt/now/test/HomeFragment.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.sopt.now.R
 import com.sopt.now.databinding.FragmentHomeBinding
+import com.sopt.now.test.data.Friend
 import com.sopt.now.test.data.HomeViewModel
+import com.sopt.now.test.data.UserPreference
 import com.sopt.now.test.friend.FriendAdapter
 
 class HomeFragment: Fragment() {
@@ -17,6 +20,7 @@ class HomeFragment: Fragment() {
         get() = requireNotNull(_binding) { "바인딩 객체 좀 생성해주세요 제발!!" }
 
     private val viewModel by viewModels<HomeViewModel>()
+    private lateinit var userPreference: UserPreference
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,7 +34,24 @@ class HomeFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        userPreference = UserPreference(requireContext())
+
+        setFriendList()
         setRecyclerView()
+    }
+
+    // 리스트에 사용자 정보 추가
+    private fun setFriendList(){
+        val userData = userPreference.getUserData()
+
+        userData?.let {
+            val newFriend = Friend(
+                profileImage = R.drawable.iv_user_profile,
+                name = it.userName,
+                selfDescription = it.selfDescription
+            )
+            viewModel.mockFriendList.add(0, newFriend)
+        }
     }
 
     // FriendAdapter 연결
