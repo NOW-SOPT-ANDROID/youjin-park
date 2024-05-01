a5274baec1553f169f266f025c01bdaa2077a390
youjin09222 <youjin09222@gmail.com>
2024-05-02 04:58:37 +0900

    refactor/#5: LoginActivity의 데이터 저장 방법 변경

--- app/src/main/java/com/sopt/now/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/LoginActivity.kt
@@ -6,27 +6,22 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.sopt.now.databinding.ActivityLoginBinding
 import com.sopt.now.test.data.UserData
-import com.sopt.now.test.data.UserViewModel
-import com.sopt.now.test.data.UserViewModelFactory
+import com.sopt.now.test.data.UserPreference
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var viewModel: UserViewModel
-    private var userData: UserData ?= null
     private lateinit var signUpLauncher: ActivityResultLauncher<Intent>
+    private lateinit var userPreference: UserPreference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val factory = UserViewModelFactory(this)
-        viewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
-
+        userPreference = UserPreference(this)
         setSignUpLauncher()
         setupLoginButton()
         setupSignUpTextView()
@@ -35,7 +30,7 @@ class LoginActivity : AppCompatActivity() {
     // 로그인
     private fun setupLoginButton() {
         binding.btnLogin.setOnClickListener {
-            userData?.let { checkLogin(it) }
+            checkLogin()
         }
     }
 
@@ -49,16 +44,19 @@ class LoginActivity : AppCompatActivity() {
     }
 
     // 로그인
-    private fun checkLogin(userData: UserData) {
+    private fun checkLogin() {
         val inputId = binding.etLoginId.text.toString()
         val inputPw = binding.etLoginPw.text.toString()
+        val userData = userPreference.getUserData()
 
         // 아이디, 비번 확인
-        if (inputId == userData.userId && inputPw == userData.userPw) {
-            showToast("로그인 성공!")
-            moveToMain()
-        } else {
-            showToast("아이디 또는 비밀번호가 잘못되었습니다.")
+        if (userData != null) {
+            if (inputId == userData.userId && inputPw == userData.userPw) {
+                showToast("로그인 성공!")
+                moveToMain()
+            } else {
+                showToast("아이디 또는 비밀번호가 잘못되었습니다.")
+            }
         }
     }
 
@@ -69,12 +67,16 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
     }
+
+    // 사용자 데이터 저장
     private fun setSignUpLauncher(){
         signUpLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ) { result ->
             if (result.resultCode == RESULT_OK) {
-                userData = viewModel.getUserData()
+                result.data?.getParcelableExtra<UserData>("userData")?.let {
+                    userPreference.saveUserData(it)
+                }
             }
         }
     }
