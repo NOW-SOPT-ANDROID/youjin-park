ad0d55f5958db6f855edd7f817f42929517dc959
youjin09222 <youjin09222@gmail.com>
2024-05-03 18:05:06 +0900

    add/#9: Interceptor 추가

--- app/src/main/java/com/sopt/now/test/data/ApiFactory.kt
+++ app/src/main/java/com/sopt/now/test/data/ApiFactory.kt
@@ -3,17 +3,44 @@ package com.sopt.now.test.data
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.sopt.now.BuildConfig
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
+import java.io.IOException
 
 object ApiFactory {
     private const val BASE_URL: String = BuildConfig.AUTH_BASE_URL
+    private lateinit var userPreference: UserPreference // UserPreference 추가
+
+    // UserPreference 초기화
+    fun initializeUserPreference(userPreference: UserPreference) {
+        this.userPreference = userPreference
+    }
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(provideOkHttpClient(HeaderInterceptor()))
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
+    }
+
+    private fun provideOkHttpClient(interceptor: HeaderInterceptor): OkHttpClient
+            = OkHttpClient.Builder().run {
+        addInterceptor(interceptor)
+        build()
+    }
+
+    class HeaderInterceptor : Interceptor {
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain) : Response = with(chain) {
+            val newRequest = request().newBuilder()
+                .addHeader("memberId", userPreference.getUserId().toString())
+                .build()
+            proceed(newRequest)
+        }
     }
 
     inline fun <reified T> create(): T = retrofit.create(T::class.java)
@@ -21,4 +48,5 @@ object ApiFactory {
 
 object ServicePool {
     val authService = ApiFactory.create<AuthService>()
+    val userService = ApiFactory.create<UserService>()
 }
---
+++ app/src/main/java/com/sopt/now/test/data/UserService.kt
@@ -0,0 +1,11 @@
+package com.sopt.now.test.data
+
+import com.sopt.now.test.data.dto.response.ResponseUserInfoDto
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface UserService {
+    @GET("member/info")
+    fun userInfo(
+    ): Call<ResponseUserInfoDto>
+}
