5a2fd43c1d4d03073593098f2ead7168de8730b4
youjin09222 <youjin09222@gmail.com>
2024-05-03 18:01:31 +0900

    feat/#9: 로그인 서버 통신 구현

--- app/src/main/java/com/sopt/now/test/presentation/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/test/presentation/LoginActivity.kt
@@ -3,26 +3,27 @@ package com.sopt.now.test.presentation
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.now.databinding.ActivityLoginBinding
-import com.sopt.now.test.data.UserData
+import com.sopt.now.test.data.ApiFactory
 import com.sopt.now.test.data.UserPreference
+import com.sopt.now.test.data.dto.request.RequestLoginDto
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
-    private lateinit var signUpLauncher: ActivityResultLauncher<Intent>
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+    private val viewModel by viewModels<LoginViewModel>()
     private lateinit var userPreference: UserPreference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         userPreference = UserPreference(this)
-        setSignUpLauncher()
+        ApiFactory.initializeUserPreference(userPreference)
+
+        initObserver()
         setupLoginButton()
         setupSignUpTextView()
     }
@@ -30,34 +31,31 @@ class LoginActivity : AppCompatActivity() {
     // 로그인
     private fun setupLoginButton() {
         binding.btnLogin.setOnClickListener {
-            checkLogin()
+            viewModel.login(getLoginRequestDto())
         }
     }
 
-    // 회원가입 페이지로 이동
-    private fun setupSignUpTextView() {
-        binding.tvSignUp.setOnClickListener {
-            Intent(this, SignUpActivity::class.java).also {
-                signUpLauncher.launch(it) // ActivityResultLauncher 실행
-            }
-        }
-    }
-
-    // 로그인
-    private fun checkLogin() {
-        val inputId = binding.etLoginId.text.toString()
-        val inputPw = binding.etLoginPw.text.toString()
-        val userData = userPreference.getUserData()
-
-        // 아이디, 비번 확인
-        if (userData != null) {
-            if (inputId == userData.userId && inputPw == userData.userPw) {
-                showToast("로그인 성공!")
+    private fun initObserver() {
+        // 사용자 데이터 저장
+        viewModel.userIdLiveData.observe(this) { userId ->
+            userId?.let {
+                userPreference.saveUserId(userId)
                 moveToMain()
-            } else {
-                showToast("아이디 또는 비밀번호가 잘못되었습니다.")
             }
         }
+
+        viewModel.liveData.observe(this) { response ->
+            showToast(response.message)
+        }
+    }
+
+    private fun getLoginRequestDto(): RequestLoginDto {
+        val id = binding.etLoginId.text.toString()
+        val password = binding.etLoginPw.text.toString()
+        return RequestLoginDto(
+            authenticationId = id,
+            password = password
+        )
     }
 
     // 메인 페이지로 이동
@@ -68,15 +66,12 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    // 사용자 데이터 저장
-    private fun setSignUpLauncher(){
-        signUpLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) { result ->
-            if (result.resultCode == RESULT_OK) {
-                result.data?.getParcelableExtra<UserData>("userData")?.let {
-                    userPreference.saveUserData(it)
-                }
+    // 회원가입 페이지로 이동
+    private fun setupSignUpTextView() {
+        binding.tvSignUp.setOnClickListener {
+            Intent(this, SignUpActivity::class.java).apply {
+                startActivity(this)
+                finish()
             }
         }
     }
