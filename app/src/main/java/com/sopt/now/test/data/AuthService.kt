905ac81b1b787ae0d59b6495783180c5621c4bec
youjin09222 <youjin09222@gmail.com>
2024-05-03 17:15:28 +0900

    feat/#9: signUp, login 인터페이스 추가

---
+++ app/src/main/java/com/sopt/now/test/data/AuthService.kt
@@ -0,0 +1,20 @@
+package com.sopt.now.test.data
+
+import com.sopt.now.test.data.dto.request.RequestLoginDto
+import com.sopt.now.test.data.dto.request.RequestSignUpDto
+import com.sopt.now.test.data.dto.response.ResponseAuthDto
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("member/join")
+    fun signUp(
+        @Body request: RequestSignUpDto,
+    ): Call<ResponseAuthDto>
+
+    @POST("member/login")
+    fun login(
+        @Body request: RequestLoginDto,
+    ): Call<ResponseAuthDto>
+}
