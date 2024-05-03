230598d47b68c65e57533242c324b003bb765fd9
youjin09222 <youjin09222@gmail.com>
2024-05-03 23:29:14 +0900

    feat/#11: UserPreference 추가

---
+++ app/src/main/java/com/sopt/now/compose/data/UserPreference.kt
@@ -0,0 +1,48 @@
+package com.sopt.now.compose.data
+
+import android.content.Context
+
+class UserPreference(context: Context) {
+    private val sharedPreferences = context.getSharedPreferences("userData", Context.MODE_PRIVATE)
+
+    // 사용자 아이디 저장
+    fun saveUserId(userId: String) {
+        with(sharedPreferences.edit()){
+            putString("userId", userId)
+            apply()
+        }
+    }
+
+    // 사용자 데이터 가져오기
+    fun getUserId(): String? {
+        with(sharedPreferences){
+            val userId = getString("userId", null)
+            return userId
+        }
+    }
+
+    // 사용자 데이터 저장
+    fun saveUserData(userData: UserData) {
+        with(sharedPreferences.edit()){
+            putString("userId", userData.userId)
+            putString("userName", userData.userName)
+            putString("userPhone", userData.userPhone)
+            apply()
+        }
+    }
+
+    // 사용자 데이터 가져오기
+    fun getUserData(): UserData? {
+        with(sharedPreferences){
+            val userId = getString("userId", null)
+            val userName = getString("userName", null)
+            val userPhone = getString("userPhone", null)
+
+            return if (userId != null && userName != null && userPhone != null) {
+                UserData(userId, userName, userPhone)
+            } else {
+                null
+            }
+        }
+    }
+}
