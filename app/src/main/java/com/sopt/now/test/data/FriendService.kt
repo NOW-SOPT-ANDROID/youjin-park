96abc70b2bd108fdbec7b3bcaceb43c24684721e
youjin09222 <youjin09222@gmail.com>
2024-05-08 16:23:17 +0900

    feat/#9: FriendService 추가

---
+++ app/src/main/java/com/sopt/now/test/data/FriendService.kt
@@ -0,0 +1,13 @@
+package com.sopt.now.test.data
+
+import com.sopt.now.test.data.dto.response.ResponseFriendDto
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface FriendService {
+    @GET("users")
+    fun friendInfo(
+        @Query("page") page: Int,
+    ): Call<ResponseFriendDto>
+}
