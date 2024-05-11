a998575160adc7b81c9d7df8c90f13cb7baebbbe
youjin09222 <youjin09222@gmail.com>
2024-05-11 10:08:57 +0900

    refactor/#11: @Serializable로 변경

--- app/src/main/java/com/sopt/now/compose/data/userData.kt
+++ app/src/main/java/com/sopt/now/compose/data/userData.kt
@@ -2,10 +2,11 @@ package com.sopt.now.compose.data
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import kotlinx.serialization.Serializable
 
-@Parcelize
+@Serializable
 data class UserData(
     val userId: String,
     val userName: String,
     val userPhone: String
-): Parcelable
+)
