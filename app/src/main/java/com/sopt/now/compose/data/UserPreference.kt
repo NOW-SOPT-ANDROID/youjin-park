9223db8fb92011274b4b99360a637d834112f58e
youjin09222 <youjin09222@gmail.com>
2024-05-11 10:01:26 +0900

    refactor/#11: shared preferences의 edit 사용

--- app/src/main/java/com/sopt/now/compose/data/UserPreference.kt
+++ app/src/main/java/com/sopt/now/compose/data/UserPreference.kt
@@ -2,32 +2,29 @@ package com.sopt.now.compose.data
 
 import android.content.Context
 
+import androidx.core.content.edit
+
 class UserPreference(context: Context) {
     private val sharedPreferences = context.applicationContext.getSharedPreferences("userData", Context.MODE_PRIVATE)
 
     // 사용자 아이디 저장
     fun saveUserId(userId: String) {
-        with(sharedPreferences.edit()){
+        sharedPreferences.edit {
             putString("userId", userId)
-            apply()
         }
     }
 
-    // 사용자 데이터 가져오기
+    // 사용자 아이디 가져오기
     fun getUserId(): String? {
-        with(sharedPreferences){
-            val userId = getString("userId", null)
-            return userId
-        }
+        return sharedPreferences.getString("userId", null)
     }
 
     // 사용자 데이터 저장
     fun saveUserData(userData: UserData) {
-        with(sharedPreferences.edit()){
+        sharedPreferences.edit {
             putString("userId", userData.userId)
             putString("userName", userData.userName)
             putString("userPhone", userData.userPhone)
-            apply()
         }
     }
 
