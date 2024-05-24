54bbdf70b9f3c01fea8b1015b9003607d89a94a6
youjin09222 <youjin09222@gmail.com>
2024-05-25 05:51:49 +0900

    refactor/#9: initGetUserInfoObserver 수정

--- app/src/main/java/com/sopt/now/test/presentation/MyPageFragment.kt
+++ app/src/main/java/com/sopt/now/test/presentation/MyPageFragment.kt
@@ -1,23 +1,22 @@
 package com.sopt.now.test.presentation
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.sopt.now.databinding.FragmentMypageBinding
-import com.sopt.now.test.data.UserData
-import com.sopt.now.test.data.UserPreference
+import com.sopt.now.test.core.view.UiState
+import com.sopt.now.test.data.dto.response.ResponseUserInfoDto
+import timber.log.Timber
 
 class MyPageFragment : Fragment() {
     private var _binding: FragmentMypageBinding? = null
     private val binding: FragmentMypageBinding
         get() = requireNotNull(_binding) { "바인딩 객체 좀 생성해주세요 제발!!" }
 
-    private val viewModel by viewModels<UserInfoViewModel>()
-    private lateinit var userPreference: UserPreference
+    private val userViewModel by viewModels<UserInfoViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,35 +29,26 @@ class MyPageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        userPreference = UserPreference(requireContext())
-        initObserver()
+        initGetUserInfoObserver()
     }
 
-    private fun initObserver() {
-        viewModel.userInfoLiveData.observe(requireActivity()) { userData ->
-            Log.d("userPreference before", "${userPreference.getUserData()}")
-            userData?.let {
-                val userData = UserData(
-                    userId = userData.data.authenticationId,
-                    userName = userData.data.nickname,
-                    userPhone = userData.data.phone
-                )
-                userPreference.saveUserData(userData)
-                setupUserData()
-                Log.d("userPreference after", "${userPreference.getUserData()}")
+    // 사용자 프로필 설정
+    private fun initGetUserInfoObserver() {
+        userViewModel.getUserInfoLiveData.observe(requireActivity()) {
+            when (it) {
+                is UiState.Success -> initMyProfile(it.data)
+                is UiState.Failure -> Timber.d("실패 : $it")
+                is UiState.Loading -> Timber.d("로딩 중")
             }
         }
     }
 
     // 받아온 UserData 적용
-    private fun setupUserData() {
-        val userData = userPreference.getUserData()
-        if (userData != null) {
-            with(binding) {
-                tvMyId.text = userData.userId
-                tvMyName.text = userData.userName
-                tvMyPhone.text = userData.userPhone
-            }
+    private fun initMyProfile(userData : ResponseUserInfoDto) {
+        with(binding) {
+            tvMyId.text = userData.data.authenticationId
+            tvMyName.text = userData.data.nickname
+            tvMyPhone.text = userData.data.phone
         }
     }
 
