8ca59970306939769b83ece63f98f46b5da4a100
youjin09222 <youjin09222@gmail.com>
2024-05-25 05:46:18 +0900

    refactor/#9: SignUpViewModel 코루틴으로 변경

--- app/src/main/java/com/sopt/now/test/presentation/SignUpViewModel.kt
+++ app/src/main/java/com/sopt/now/test/presentation/SignUpViewModel.kt
@@ -2,55 +2,25 @@ package com.sopt.now.test.presentation
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.gson.Gson
+import androidx.lifecycle.viewModelScope
+import com.sopt.now.test.core.view.UiState
 import com.sopt.now.test.data.ServicePool
-import com.sopt.now.test.data.BaseState
 import com.sopt.now.test.data.dto.request.RequestSignUpDto
 import com.sopt.now.test.data.dto.response.ResponseAuthDto
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.launch
 
 class SignUpViewModel : ViewModel() {
-    private val authService by lazy { ServicePool.authService }
-    val liveData = MutableLiveData<BaseState>()
+    private val _postSignUpLiveData: MutableLiveData<UiState<ResponseAuthDto>> = MutableLiveData()
+    val postSignUpLiveData: MutableLiveData<UiState<ResponseAuthDto>> = _postSignUpLiveData
 
-    fun signUp(request: RequestSignUpDto) {
-        authService.postSignUp(request).enqueue(object : Callback<ResponseAuthDto> {
-            override fun onResponse(
-                call: Call<ResponseAuthDto>,
-                response: Response<ResponseAuthDto>,
-            ) {
-                if (response.isSuccessful) {
-                    val userId = response.headers()["location"]
-                    liveData.value = BaseState(
-                        isSuccess = true,
-                        message = "회원가입 성공 유저의 ID는 $userId 입니다."
-                    )
-                } else {
-                    val error = response.errorBody()?.string()
-                    val gson = Gson()
-                    try {
-                        val errorResponse = gson.fromJson(error, ResponseAuthDto::class.java)
-                        liveData.value = BaseState(
-                            isSuccess = false,
-                            message = "회원가입 실패: ${errorResponse.message}" // 에러 메시지 사용
-                        )
-                    } catch (e: Exception) {
-                        liveData.value = BaseState(
-                            isSuccess = false,
-                            message = "회원가입 실패: 에러 메시지 파싱 실패"
-                        )
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseAuthDto>, t: Throwable) {
-                liveData.value = BaseState(
-                    isSuccess = false,
-                    message = "서버 에러"
-                )
-            }
-        })
+    fun postSignUp(requestSignUp: RequestSignUpDto) {
+        viewModelScope.launch {
+            runCatching {
+                ServicePool.authService.postSignUp(requestSignUp)
+            }.fold(
+                { _postSignUpLiveData.value = UiState.Success(it) },
+                { _postSignUpLiveData.value = UiState.Failure(it.message.toString()) }
+            )
+        }
     }
 }
