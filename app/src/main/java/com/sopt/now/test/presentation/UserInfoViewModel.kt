9873cc6ce83ff19e74ca45080b40a1d5de6c3b7e
youjin09222 <youjin09222@gmail.com>
2024-05-25 05:50:38 +0900

    refactor/#9: UserInfoViewModel 코루틴으로 변경

--- app/src/main/java/com/sopt/now/test/presentation/UserInfoViewModel.kt
+++ app/src/main/java/com/sopt/now/test/presentation/UserInfoViewModel.kt
@@ -2,62 +2,28 @@ package com.sopt.now.test.presentation
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.gson.Gson
+import androidx.lifecycle.viewModelScope
+import com.sopt.now.test.core.view.UiState
 import com.sopt.now.test.data.ServicePool
-import com.sopt.now.test.data.BaseState
-import com.sopt.now.test.data.dto.response.ResponseAuthDto
 import com.sopt.now.test.data.dto.response.ResponseUserInfoDto
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.launch
 
 class UserInfoViewModel : ViewModel() {
-    private val userService by lazy { ServicePool.userService }
-    val liveData = MutableLiveData<BaseState>()
-    val userInfoLiveData = MutableLiveData<ResponseUserInfoDto?>()
+    private val _getUserInfoLiveData: MutableLiveData<UiState<ResponseUserInfoDto>> = MutableLiveData()
+    val getUserInfoLiveData: MutableLiveData<UiState<ResponseUserInfoDto>> = _getUserInfoLiveData
 
     init{
-        userInfo()
+        getUserInfo()
     }
 
-    private fun userInfo() {
-        userService.getUserInfo().enqueue(object : Callback<ResponseUserInfoDto> {
-            override fun onResponse(
-                call: Call<ResponseUserInfoDto>,
-                response: Response<ResponseUserInfoDto>,
-            ) {
-                if (response.isSuccessful) {
-                    val data: ResponseUserInfoDto? = response.body()
-                    userInfoLiveData.value = data
-
-                    liveData.value = BaseState(
-                        isSuccess = true,
-                        message = response.message()
-                    )
-                } else {
-                    val error = response.errorBody()?.string()
-                    val gson = Gson()
-                    try {
-                        val errorResponse = gson.fromJson(error, ResponseAuthDto::class.java)
-                        liveData.value = BaseState(
-                            isSuccess = false,
-                            message = "회원 정보 조회 실패: ${errorResponse.message}" // 에러 메시지 사용
-                        )
-                    } catch (e: Exception) {
-                        liveData.value = BaseState(
-                            isSuccess = false,
-                            message = "회원 정보 조회 실패: 에러 메시지 파싱 실패"
-                        )
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseUserInfoDto>, t: Throwable) {
-                liveData.value = BaseState(
-                    isSuccess = false,
-                    message = "서버 에러"
-                )
-            }
-        })
+    fun getUserInfo() {
+        viewModelScope.launch {
+            runCatching {
+                ServicePool.userService.getUserInfo()
+            }.fold(
+                { _getUserInfoLiveData.value = UiState.Success(it) },
+                { _getUserInfoLiveData.value = UiState.Failure(it.message.toString()) }
+            )
+        }
     }
 }
