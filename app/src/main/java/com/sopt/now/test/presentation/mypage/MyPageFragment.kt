758e5ab84cf1bc6e09f426fa8de5b71f73696093
youjin09222 <youjin09222@gmail.com>
2024-06-07 08:21:08 +0900

    mod/#9 : 로그 메시지 추출하여 적용 (MyPageFragment)

--- app/src/main/java/com/sopt/now/test/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sopt/now/test/presentation/mypage/MyPageFragment.kt
@@ -1,4 +1,4 @@
-package com.sopt.now.test.presentation
+package com.sopt.now.test.presentation.mypage
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.sopt.now.R
 import com.sopt.now.databinding.FragmentMypageBinding
+import com.sopt.now.test.core.util.fragment.showToast
 import com.sopt.now.test.core.view.UiState
 import com.sopt.now.test.data.dto.response.ResponseUserInfoDto
+import com.sopt.now.test.presentation.home.UserProfileViewModel
 import timber.log.Timber
 
 class MyPageFragment : Fragment() {
@@ -16,7 +19,7 @@ class MyPageFragment : Fragment() {
     private val binding: FragmentMypageBinding
         get() = requireNotNull(_binding) { "바인딩 객체 좀 생성해주세요 제발!!" }
 
-    private val userViewModel by viewModels<UserInfoViewModel>()
+    private val userProfileViewModel by viewModels<UserProfileViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,22 +32,22 @@ class MyPageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initGetUserInfoObserver()
+        initGetUserObserver()
     }
 
     // 사용자 프로필 설정
-    private fun initGetUserInfoObserver() {
-        userViewModel.getUserInfoLiveData.observe(requireActivity()) {
+    private fun initGetUserObserver() {
+        userProfileViewModel.getUserInfoProfileLiveData.observe(viewLifecycleOwner) {
             when (it) {
                 is UiState.Success -> initMyProfile(it.data)
-                is UiState.Failure -> Timber.d("실패 : $it")
-                is UiState.Loading -> Timber.d("로딩 중")
+                is UiState.Failure -> showToast(it.errorMessage)
+                is UiState.Loading -> Timber.d(getString(R.string.message_loading))
             }
         }
     }
 
     // 받아온 UserData 적용
-    private fun initMyProfile(userData : ResponseUserInfoDto) {
+    private fun initMyProfile(userData: ResponseUserInfoDto) {
         with(binding) {
             tvMyId.text = userData.data.authenticationId
             tvMyName.text = userData.data.nickname
