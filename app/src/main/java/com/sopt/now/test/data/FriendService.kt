9a0ead40339d6079bc27f61642dcbf8da7a2a280
youjin09222 <youjin09222@gmail.com>
2024-06-07 08:15:44 +0900

    refactor/#9 : getFriendInfo를 suspend 함수로 변경

--- app/src/main/java/com/sopt/now/test/data/FriendService.kt
+++ app/src/main/java/com/sopt/now/test/data/FriendService.kt
@@ -1,13 +1,13 @@
 package com.sopt.now.test.data
 
 import com.sopt.now.test.data.dto.response.ResponseFriendDto
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface FriendService {
+    // 친구 프로필
     @GET("users")
-    fun getFriendInfo(
+    suspend fun getFriendInfo(
         @Query("page") page: Int,
-    ): Call<ResponseFriendDto>
+    ): ResponseFriendDto
 }
