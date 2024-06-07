acb32da006e14dd6f16feddfad4df82ad4072552
youjin09222 <youjin09222@gmail.com>
2024-06-07 17:43:37 +0900

    REFACTOR/#17: LoginViewModel에서 LoginRepository 사용

--- app/src/main/java/com/sopt/now/test/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/sopt/now/test/presentation/login/LoginViewModel.kt
@@ -1,39 +1,25 @@
 package com.sopt.now.test.presentation.login
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.now.test.core.view.UiState
-import com.sopt.now.test.data.ApiFactory
-import com.sopt.now.test.data.ServicePool
 import com.sopt.now.test.data.dto.request.RequestLoginDto
 import com.sopt.now.test.data.dto.response.ResponseAuthDto
+import com.sopt.now.test.data.repository.LoginRepository
 import kotlinx.coroutines.launch
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(
+    private val loginRepository: LoginRepository
+) : ViewModel() {
     private val _postLoginLiveData: MutableLiveData<UiState<ResponseAuthDto>> = MutableLiveData()
-    val postLoginLiveData: MutableLiveData<UiState<ResponseAuthDto>> = _postLoginLiveData
+    val postLoginLiveData: LiveData<UiState<ResponseAuthDto>> = _postLoginLiveData
 
     fun postLogin(request: RequestLoginDto) {
         viewModelScope.launch {
-            runCatching {
-                ServicePool.authService.postLogin(request)
-            }.fold(
-                {
-                    val response = ServicePool.authService.postLogin(request)
-                    val userId = response.headers()["location"]
-                    if (userId != null) {
-                        ApiFactory.userPreference.saveUserId(userId)
-                    }
-                    _postLoginLiveData.value = UiState.Success(
-                        response.body() ?: ResponseAuthDto(
-                            -1,
-                            "Empty response body"
-                        )
-                    )
-                },
-                { _postLoginLiveData.value = UiState.Failure(it.message.toString()) }
-            )
+            val result = loginRepository.postLogin(request)
+            _postLoginLiveData.value = result
         }
     }
 }
